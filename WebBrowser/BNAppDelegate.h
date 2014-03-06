670faa49a4978e099f0e83c71d32abac4828abba
DukeDouglas <DukeDouglas@DukeDouglasdeMacBook-Pro.local>
2014-03-06 09:37:48 +0800

    Initial Commit

---
+++ WebBrowser/BNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNAppDelegate.h
+//  WebBrowser
+//
+//  Created by DukeDouglas on 14-3-6.
+//  Copyright (c) 2014年 北京蓝鸥科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WebBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WebBrowser
+//
+//  Created by DukeDouglas on 14-3-6.
+//  Copyright (c) 2014年 北京蓝鸥科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNAppDelegate class]));
+    }
+}
